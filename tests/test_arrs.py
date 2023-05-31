dbf310b955bb8752d647804ed1e856bc1627fadf
Maxim Shurygin <maximys2142@mail.ru>
2023-05-31 20:40:57 +0800

    added tests to 100% coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,6 +4,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -2, "test") == "test"
     with pytest.raises(IndexError):
         arrs.get([], 0, "test")
 
@@ -11,3 +12,6 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3, 4], -6) == [1, 2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
